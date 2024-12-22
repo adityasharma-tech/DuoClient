7ad084f4df708ea8eb5b88fff8787d6a21969dbf
Aditya Sharma <aadisharma.in+github@gmail.com>
2024-12-22 17:17:48 +0530

    Add HomeScreen component and set up navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 function App(): React.JSX.Element {
 
+    const Stack = createNativeStackNavigator();
+
   return (
-    
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
