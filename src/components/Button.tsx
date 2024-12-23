a259dcb984d53eabf3c4d55305d47e474a653ffc
Aditya Sharma <aadisharma.in+github@gmail.com>
2024-12-23 14:30:22 +0530

    Add react-native-tcp-socket for TCP server functionality and update PrimaryButton component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,47 @@
-import { GestureResponderEvent, Text, TextStyle, TouchableOpacity, ViewStyle } from 'react-native'
-import React, { PropsWithChildren } from 'react'
+import {
+  GestureResponderEvent,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
+import React, {PropsWithChildren} from 'react';
 
-export function PrimaryButton({children, onPress, containerStyle={}, textStyle={}}: PropsWithChildren<{children: string; onPress: (event: GestureResponderEvent) => void, containerStyle?: ViewStyle, textStyle?: TextStyle }>) {
+export function PrimaryButton({
+  children,
+  onPress,
+  containerStyle = {},
+  textStyle = {},
+}: PropsWithChildren<{
+  children: string;
+  onPress: (event: GestureResponderEvent) => void;
+  containerStyle?: ViewStyle;
+  textStyle?: TextStyle;
+}>) {
   return (
-    <TouchableOpacity onPress={onPress} style={[containerStyle,{
-        paddingHorizontal: 48,
-        display: 'flex',
-        paddingVertical: 10,
-        backgroundColor: 'black',
-        borderRadius: 10,
-    }]}>
-      <Text style={[textStyle, {
-        color: 'white',
-        textAlign: 'center'
-      }]}>{children}</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.7}
+      style={[
+        containerStyle,
+        {
+          paddingHorizontal: 48,
+          display: 'flex',
+          paddingVertical: 10,
+          backgroundColor: 'black',
+          borderRadius: 10,
+        },
+      ]}>
+      <Text
+        style={[
+          textStyle,
+          {
+            color: 'white',
+            textAlign: 'center',
+          },
+        ]}>
+        {children}
+      </Text>
     </TouchableOpacity>
-  )
+  );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,67 @@
 import {View} from 'react-native';
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import {RNCamera} from 'react-native-camera';
-import { PrimaryButton } from '../components/Button';
+import {PrimaryButton} from '../components/Button';
+import TcpSocket from 'react-native-tcp-socket';
 
 export default function HomeScreen() {
   const cameraRef = useRef<RNCamera>(null);
-  
+  const [server, setServer] = useState<any>(null);
+
+  React.useEffect(() => {
+    const tcpServer = TcpSocket.createServer(function (socket) {
+      console.log('Client connected: ', socket.address());
+
+      socket.write('Camera stream started\n');
+
+      socket.on('data', data => {
+        console.log('Data received:', data.toString());
+      });
+
+      socket.on('error', error => {
+        console.error('Socket error:', error);
+      });
+
+      socket.on('close', () => {
+        console.log('Connection closed');
+      });
+    });
+    tcpServer.listen({port: 8002, host: '0.0.0.0'}, () => {
+      console.log('server is running on port 8002');
+    });
+
+    setServer(tcpServer);
+
+    return () => {
+      tcpServer.close();
+    };
+  }, []);
+
+  const sendFrame = async () => {
+    if(cameraRef.current){
+      const data = await cameraRef.current.takePictureAsync({ base64: true })
+      if(!data.base64) return;
+      const frame = Buffer.from(data.base64, 'base64');
+      if(server) {
+        server.write(frame)
+      }
+    }
+  }
+
   return (
     <View>
-      <View style={{
-        width: 1080*0.3,
-        height: 1920*0.3,
-        alignSelf: 'center',
-        overflow: 'hidden',
-        position:'relative',
-        borderRadius: 5
-      }}>
+      <View
+        style={{
+          width: 1080 * 0.3,
+          height: 1920 * 0.3,
+          alignSelf: 'center',
+          overflow: 'hidden',
+          position: 'relative',
+          borderRadius: 5,
+        }}>
         <RNCamera
+          captureAudio={false}
+          ref={cameraRef}
           type="back"
           style={{
             width: '100%',
@@ -24,13 +69,13 @@ export default function HomeScreen() {
           }}
         />
       </View>
-      <View style={{
-        marginTop: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-      }}>
-        
-      <PrimaryButton onPress={()=>{}}>Start Server</PrimaryButton>
+      <View
+        style={{
+          marginTop: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}>
+        <PrimaryButton onPress={() => {}}>Start Server</PrimaryButton>
       </View>
     </View>
   );
