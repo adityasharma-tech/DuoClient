f9bd8af00f868ebee6464ddd4150ed8506bb9d81
Aditya Sharma <aadisharma.in+github@gmail.com>
2024-12-23 12:16:51 +0530

    Integrate react-native-camera and update permissions in AndroidManifest

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text, Button } from 'react-native'
+import { View, Button } from 'react-native'
 import React from 'react'
+import { RNCamera } from 'react-native-camera'
 
 export default function HomeScreen() {
   return (
     <View>
-      
+      <RNCamera type='back'/>
       <Button title='Start server'/>
     </View>
   )
